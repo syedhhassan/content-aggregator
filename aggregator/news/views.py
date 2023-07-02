9f042520a8a2cb19bdd0e7260e00a5f86d4f2387
syedhhassan <mbsyedhassan@gmail.com>
2023-07-03 03:59:05 +0530

    Index page

--- aggregator/news/views.py
+++ aggregator/news/views.py
@@ -3,18 +3,18 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 
+def news(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    new = soup.find_all('h3')
+    news = []
+    for i in new:
+        news.append(i.text)
+    return news
+
 # Create your views here.
-#def news(url):
-sports_req = requests.get("https://www.nytimes.com/section/sports")
-sports_soup = BeautifulSoup(sports_req.content, 'html.parser')
-sports_new = sports_soup.find_all('h3')
-sports_img = sports_soup.find_all('img')
-sports_news = []
-sports_images = []
-for i in sports_new:
-    sports_news.append(i.text)
-for j in sports_img:
-    sports_images.append(j.content)
     
 def sports(request):
-    return render(request, 'index.html', {"sports_images": sports_images, "sports_news": sports_news} )
+    url = "https://www.nytimes.com/section/sports"
+    received_news = news(url)    
+    return render(request, 'index.html', {"news": received_news} )
